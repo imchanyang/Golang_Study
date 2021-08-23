aebe79f10b4226085446c34f14957d4c05bbde11
imchanyang <cksdid636@naver.com>
2021-08-23 15:37:40 +0900

    second commit

--- Golang3.go
+++ Golang3.go
@@ -1,1 +1,59 @@
 package main
+
+func main() {
+
+	println(add(2, 3))
+
+	msg := "Hello"
+	say(&msg)
+	println(msg)
+
+	say_("This", "is", "a", "book")
+	say_("Hi")
+
+	count, total := sum(1, 7, 3, 5, 9)
+	println(count, total)
+
+	count_, total_ := sum_(1, 7, 3, 5, 9)
+	println(count_, total_)
+}
+
+// 함수 변수타입, 반환 타입 쓰는 법
+// Pass By Value
+func add(a int, b int) int {
+	return a + b
+}
+
+// Pass By Reference
+func say(msg *string) {
+	println(*msg)
+	*msg = "Changed"
+}
+
+// 가변 인자 함수
+// ...(3개의 마침표를 사용) 0개 ~ n개의 동일 타입 파라미터를 전달할 수 있다
+func say_(msg ...string) {
+	for _, s := range msg {
+		println(s)
+	}
+}
+
+// 복수 개의 값을 리턴하는 함수
+func sum(nums ...int) (int, int) {
+	s := 0
+	count := 0
+	for _, n := range nums {
+		s += n
+		count++
+	}
+	return count, s
+}
+
+// Named Return Parameter
+func sum_(nums ...int) (count int, total int) {
+	for _,n := range nums {
+		total += n
+	}
+	count = len(nums)
+	return
+}
--- Golang4.go
+++ Golang4.go
@@ -1,1 +1,77 @@
 package main
+
+func main() {
+	// 익명 함수, Anonymous Function
+	sum := func(n ...int) int {
+		s := 0
+		for _, i := range n{
+			s += i
+		}
+		return s
+	}
+
+	result := sum(1, 2, 3, 4, 5)
+	println(result)
+
+
+	// 일급함수
+	// 함수는 기본 타입과 동일하게 취급
+	// 다른 함수의 파라미터로 전달하거나 다른 함수의 리턴 값으로 사용될 수 있다
+
+	//변수 add 에 익명함수 할당
+	add := func(i int, j int) int {
+		return i + j
+	}
+
+	// add 함수 전달
+	r1 := calc(add, 10, 20)
+	println(r1)
+
+	// 직접 첫번째 파라미터에 익명함수를 정의함
+	r2 := calc(func(x int, y int) int { return x + y }, 10, 20)
+	println(r2)
+
+	// type문
+	r3 := calc_(func(x int, y int) int {return x + y}, 10, 20)
+	println(r3)
+
+	//클로저 사용
+	next := nextValue()
+
+	println(next())  // 1
+	println(next())  // 2
+	println(next())  // 3
+
+	anotherNext := nextValue()
+	println(anotherNext()) // 1 다시 시작
+	println(anotherNext()) // 2
+
+
+
+}
+
+func calc(f func(int, int) int, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+
+//type문을 사용한 함수 원형 정의
+
+// 원형 정의
+type calculator func(int, int) int
+
+// calculator 원형 사용
+func calc_(f calculator, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+
+// 클로저 정의(Closure)
+func nextValue() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
